e33f387bfb008a6a0e503e4866090ed285c03b06
mdxs <van.wijgerden@mdxs.net>
2026-05-19 03:01:33 +0200

    Added pre-commit exclude for some Bruno and Postman collections

--- scripts/bad_code.py
+++
@@ -1,6 +0,0 @@
-mport os # Unused import
-import sys # Unused import
-
-def messy_function():
-    x = "this is a very long string that should definitely exceed your 100 character line length limit because we are testing ruff"
-    print(x)
