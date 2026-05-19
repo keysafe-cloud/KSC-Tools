839eb482e5fa56744a03e41fe6f80aab24451ab7
mdxs <van.wijgerden@mdxs.net>
2026-05-19 02:30:23 +0200

    test: testing pre-commit failure (expected)

---
+++ scripts/bad_code.py
@@ -0,0 +1,6 @@
+mport os # Unused import
+import sys # Unused import
+
+def messy_function():
+    x = "this is a very long string that should definitely exceed your 100 character line length limit because we are testing ruff"
+    print(x)
